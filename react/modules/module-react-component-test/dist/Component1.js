7fcf9a26f65a19bfa01d9bdec3426ecdc0ea2ac7
Phan Tung <tungpt260794@Phans-MacBook-Pro.local>
2019-10-08 08:43:24 +0700

    ass nodejs

---
+++ react/modules/module-react-component-test/dist/Component1.js
@@ -0,0 +1,17 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var _default = function _default() {
+  return _react["default"].createElement("div", null, "My React Component 1");
+};
+
+exports["default"] = _default;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9Db21wb25lbnQxLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQTs7OztlQUVlLG9CQUFNO0FBQ25CLFNBQU8sb0VBQVA7QUFDRCxDIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gXCJyZWFjdFwiO1xuXG5leHBvcnQgZGVmYXVsdCAoKSA9PiB7XG4gIHJldHVybiA8ZGl2Pk15IFJlYWN0IENvbXBvbmVudCAxPC9kaXY+O1xufTtcbiJdfQ==
---
+++ react/modules/module-react-component-test/dist/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Component1", {
+  enumerable: true,
+  get: function get() {
+    return _Component["default"];
+  }
+});
+Object.defineProperty(exports, "Component2", {
+  enumerable: true,
+  get: function get() {
+    return _Component2["default"];
+  }
+});
+
+var _Component = _interopRequireDefault(require("./Component1"));
+
+var _Component2 = _interopRequireDefault(require("./Component2"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7Ozs7QUFBQTs7QUFDQSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBDb21wb25lbnQxIGZyb20gXCIuL0NvbXBvbmVudDFcIjtcbmltcG9ydCBDb21wb25lbnQyIGZyb20gXCIuL0NvbXBvbmVudDJcIjtcblxuZXhwb3J0IHsgQ29tcG9uZW50MSwgQ29tcG9uZW50MiB9O1xuIl19
